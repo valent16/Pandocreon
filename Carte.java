fcf9ce8720bf322b0cb47eb3fc5895f50d3b6429
Lucas <LucasNoga@users.noreply.github.com>
2016-09-26 21:10:43 +0200

    debut de programme

---
+++ Carte.java
@@ -0,0 +1,19 @@
+package main;
+
+////////////A VOIR SI CELA PEUT ETRE UNE INTERFACE///////////////////
+/**Classe abstraite qui represente n'importe quel carte*/
+public abstract class Carte {
+	
+	
+	/*attributs:
+	 * - une Origin
+	 * - Un tableau ou une liste de Dogmes
+	 * - Un Sacrifice
+	 * - un entier pour le nombre de croyants 
+	 */
+	
+	/*methodes:
+	 * 	
+	 */
+
+}
