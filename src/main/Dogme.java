d39b8ec33fa7b146e6963c36b5f86ff0176a7fde
Lucas <luc4snoga@gmail.com>
2016-10-02 11:18:43 +0200

    Ajout des sources du programme

---
+++ src/main/Dogme.java
@@ -0,0 +1,14 @@
+package main;
+
+/**Classe qui représente le(s) dogme(s) de la carte*/
+public class Dogme {
+	
+	/*attributs:
+	 * -  
+	 */
+	
+	/*methodes:
+	 * 	
+	 */
+
+}
---
+++ src/main/Player.java
@@ -0,0 +1,14 @@
+package main;
+
+/**Classe qui représente un joueur*/
+public class Player {
+	
+	/*attributs:
+	 * -  Possede au depart 7 cartes dont une carte divinité
+	 */
+	
+	/*methodes:
+	 * - un methode piocher() qui recupere une carte de la pile de carte
+	 */
+
+}
