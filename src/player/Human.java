8007552329954f0e0e2a8def84360a3be75ba134
Noga Lucas <luc4snoga@gmail.com>
2016-11-12 13:48:16 +0100

    - Modification de la classe Game - Ajout de le classe Human - Modification des classe Player et Bot

---
+++ src/player/Human.java
@@ -0,0 +1,14 @@
+package player;
+
+public class Human extends Player{
+	
+	/**Constructeur de joueur qui est appelé pour créer un joueur humain*/
+	public Human(String pseudo) {
+		super(pseudo);
+	}
+	
+	public void play(){
+		
+	}
+
+}
