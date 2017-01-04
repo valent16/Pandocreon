79b8e69aa9994e346f272f4e484227f22341ad4f
Noga Lucas <luc4snoga@gmail.com>
2017-01-05 00:55:19 +0100

    Ajout du dossier images contenant certaines cartes

--- Code/src/fenetre/FenetrePrincipale.java
+++ Code/src/fenetre/FenetrePrincipale.java
@@ -1,7 +1,7 @@
 package fenetre;
 
 import java.awt.EventQueue;
-import java.awt.Image;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -11,7 +11,6 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Component;
 
 import javax.swing.JLabel;
 
@@ -19,6 +18,8 @@ public class FenetrePrincipale {
 
 	private JFrame fenetre;
 	private JButton buttonImage;
+	private JPanel EastPanel, SouthPanel, WestPanel, NorthPanel, CenterPanel;
+	private JLabel label1;
 
 	/**
 	 * Launch the application.
@@ -42,7 +43,7 @@ public class FenetrePrincipale {
 	public FenetrePrincipale() {
 		initialize();
 	}
-	
+
 	public JFrame getFenetre(){
 		return fenetre;
 	}
@@ -50,53 +51,59 @@ public class FenetrePrincipale {
 	/**
 	 * Initialize the contents of the frame.
 	 */
+	/**
+	 * 
+	 */
 	private void initialize() {
 		fenetre = new JFrame();
-		fenetre.getContentPane().setBackground(Color.GRAY);
+		ImageIcon bg = new ImageIcon("images/")
+		fenetre.getContentPane().setI
 		fenetre.setTitle("Pandocreon Divinae");
 		fenetre.setBounds(100, 100, 450, 300);
 		fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		JPanel NorthPanel = new JPanel();
+
+		NorthPanel = new JPanel();
 		NorthPanel.setBackground(Color.GRAY);
 		fenetre.getContentPane().add(NorthPanel, BorderLayout.NORTH);
-		
-		JPanel WestPanel = new JPanel();
+
+		WestPanel = new JPanel();
 		WestPanel.setBackground(Color.GRAY);
 		fenetre.getContentPane().add(WestPanel, BorderLayout.WEST);
-		
-		JPanel SouthPanel = new JPanel();
+
+		SouthPanel = new JPanel();
 		SouthPanel.setBackground(Color.GRAY);
 		fenetre.getContentPane().add(SouthPanel, BorderLayout.SOUTH);
-		
-		JPanel EastPanel = new JPanel();
+
+		EastPanel = new JPanel();
 		EastPanel.setBackground(Color.GRAY);
 		fenetre.getContentPane().add(EastPanel, BorderLayout.EAST);
-		
-		JPanel CenterPanel = new JPanel();
+
+		CenterPanel = new JPanel();
+
 		CenterPanel.setBackground(Color.GRAY);
 		fenetre.getContentPane().add(CenterPanel, BorderLayout.CENTER);
-		
-		JLabel label1 = new JLabel("A faire");
+
+		label1 = new JLabel("A faire");
 		CenterPanel.add(label1);
-		
-		
+
+
 		//Image i = new Image("./Brewalen.png");
-		ImageIcon image = new ImageIcon("./Brewalen.png");
+		ImageIcon image = new ImageIcon("./images/cartes/Brewalen.png");
 		buttonImage = new JButton(image);
 		buttonImage.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				System.out.println("Action a effectuer dessus");
-				buttonImage.repaint();
-				
+				SouthPanel.setBackground(Color.black);
+				SouthPanel.add
+				SouthPanel.repaint(); ///on met a jour le panel avec cette methode
+
 			}
 		});
 		SouthPanel.add(buttonImage);
-		
+
 		//TODO un label pour le nom du joueur dans le southPanel
 		//TODO creer quelqes caret en mode bouton pour voir si ca marche dans le southPanel avec un listenr qui active la carte
 	}
-
 }
