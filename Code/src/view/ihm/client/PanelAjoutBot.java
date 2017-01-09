426837989ba38dbecdf8f82a2c17488e683641fb
Noga Lucas <luc4snoga@gmail.com>
2017-01-09 21:22:25 +0100

    Ajout d'un nouveau package pour le client

---
+++ Code/src/view/ihm/client/PanelAjoutBot.java
@@ -0,0 +1,20 @@
+package view.ihm.client;
+import java.util.Scanner;
+
+import javax.swing.JPanel;
+
+import controller.GameController;
+import model.game.Game;
+
+/**Classe qui gere la vue de la partie en mode console*/
+public class PanelAjoutBot extends JPanel{
+
+	private static final long serialVersionUID = 1L;
+	GameController controllerJeu;
+	Game game;
+	
+	public PanelAjoutBot(){
+		
+	}
+	
+}
---
+++ Code/src/view/ihm/client/PanelConfirmation.java
@@ -0,0 +1,11 @@
+package view.ihm.client;
+
+import javax.swing.JPanel;
+
+
+//TODO PANEL QUI RECAPITULE TOUTES LES ACTIONS DE l'IHM
+public class PanelConfirmation extends JPanel{
+
+	private static final long serialVersionUID = 1L;
+
+}
