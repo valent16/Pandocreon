8da38351dfcf10c2181a74a16dd6b28aa5a99adf
Noga Lucas <luc4snoga@gmail.com>
2017-01-10 12:32:35 +0100

    modif du client

--- Code/src/view/ihm/Client.java
+++ Code/src/view/ihm/Client.java
@@ -9,6 +9,8 @@ import javax.swing.JOptionPane;
 import controller.GameController;
 import model.game.Game;
 import model.game.GameManager;
+import model.player.Bot;
+import model.player.Human;
 import view.ihm.client.PanelAjoutBot;
 import view.ihm.client.PanelAjoutDifficulte;
 import view.ihm.client.PanelAjoutHumain;
@@ -41,7 +43,7 @@ public class Client{
 
 	/**Attribut representant la strategie choisi par le joueur*/
 	private String strategie;
-	
+
 	/**Attribut representant les noms des bots*/
 	private LinkedList<String> listeNomBot = new LinkedList<String>();
 
@@ -103,234 +105,210 @@ public class Client{
 		setPanelAjoutInfoHumain(new PanelAjoutInfoHumain(this));		
 	}
 
-
 	public void confirmerPartie() {
 		setPanelConfirmation(new PanelConfirmation(this));
 	}
 
-	/**Methode pour instancier les joueurs et les ajouters dans le gameController*/
-	public void CreationJoueurs(){
-		int nombreTotalJoueur = nombreBot+nombreHumain;
-		if(nombreTotalJoueur == 0){
-			JOptionPane.showConfirmDialog(null, "Vous n'avez saisis aucun joueur", "probleme nombre de joueur", 
-					JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, logo);
-			retourMenuPrincipale();
-		}
-		else{//sinon il y au moins un joueur
-			
-			//Creation et ajout des bots dans la partie
-			for(int i = 0; i < nombreBot; i++){
-				gc.CreationBot("Bot "+Game.getBotName(i), strategie);//ajout des bots
-			}
-			System.out.println("affichage des bots "+ GameManager.getInstanceUniqueManager().getPlayers());//TODO A ENLEVER
-
-			//Creation et ajout des humains dans la partie
-			for(int i = 0; i < nombreHumain; i++){
-				gc.CreationJoueur("Humain "+Game.getBotName(i), AGE_JOUEUR);
-			}
-			System.out.println("affichage des joueur "+ GameManager.getInstanceUniqueManager().getPlayers());//TODO A ENLEVER
-		}
-	}
-
 	/**Methode pour lancer la partie*/
 	public void lancerPartie(){
-		///////////////////////////////////////////////////////////////////////TODO ICI QU'on lance LA PARTIE
+		creationPartie();
+		System.out.println("Lancement de la partie");//TODO A ENLEVER
+		//gc.startGame();
+
+		GameController gameControler = new GameController();//TODO A ENLEVER
+		gameControler.startGame();//TODO A ENLEVER
+	}
+
+
+	/**Methode pour instancier les joueurs et les ajouters dans le gameController*/
+	public void creationPartie(){
+		System.out.println("Creation de la partie");//TODO A ENLEVER
+		gc = new GameController();
+		
 		System.out.println("Instanciation des Bots");//TODO A ENLEVER
 		//Creation et ajout des bots dans la partie
 		Iterator<String> itBot = listeNomBot.iterator();
 		while(itBot.hasNext()){
 			gc.CreationBot(itBot.next(), getStrategie());//instanciation des bots
 		}
-		
+
 		System.out.println("affichage des joueurs dans la partie "+ GameManager.getInstanceUniqueManager().getPlayers());//TODO A ENLEVER
-		
+
 		System.out.println("Instanciation des Humains");//TODO A ENLEVER
+		//Creation et ajout des Humains dans la partie
 		Iterator<String> itNomHumain = listeNomHumain.iterator();
 		Iterator<Integer> itAgeHumain = listeAgeHumain.iterator();
 		while( itNomHumain.hasNext() && itAgeHumain.hasNext() ){
 			gc.CreationJoueur(itNomHumain.next(), itAgeHumain.next());//instanciation des humains
 		}
 		System.out.println("affichage des joueurs dans la partie "+ GameManager.getInstanceUniqueManager().getPlayers());//TODO A ENLEVER
-		
-		//Creation et ajout des Humains dans la partie
-		System.out.println("Lancement de la partie");
-		
-		//LA ON AJOUTE CE QU'il y a dans le main
-		
-		////////////////////////////:
-		/*GameController gc = new GameController();
-		gc.startGame();
-
-		//TODO ON peut recuperer toutes les information (noms, age et strategie dans cette classe
-		gc.CreationJoueur("valentin", 18);
-		gc.CreationJoueur("David", 20);
-
-		gc.lancerPartie();*/
-		/////////////////////////////////
 	}
 
-		/**Methode pour revenir au panel MenuPrincipale*/
-		public void retourMenuPrincipale() {
-			int option = JOptionPane.showConfirmDialog(null, "Voulez vous revenir au menu principal ?", "Revenir au Menu Principal", 
-					JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, logo);
-			if(option == JOptionPane.OK_OPTION){
-				frame.setTitle("Client Pandocreon Divinae");
-				frame.setContentPane(panelMenuPrincipale);
-				frame.repaint();
-				frame.validate();
-				//on reinitialise les données
-
-				setNombreHumain(0);
-				setNombreBot(0);
-				setStrategie("");
-				setListeNomBot(new LinkedList<String>());
-				setListeNomHumain(new LinkedList<String>());
-				setListeAgeHumain(new LinkedList<Integer>());
-				setIndexHumain(1);
-			}
-		}
-
-		/**Getter Jframe
-		 * @return la fenetre graphique
-		 */
-		public JFrame getFenetre(){
-			return frame;
-		}
-
-		/**Getter nombre de joueur
-		 * @return le nombre de joueur choisi pour demarrer la partie
-		 */
-		public int getNombreJoueurs(){
-			return this.getNombreBot()+this.getIndexHumain();
-		}
-
-		/**Getter pour le controller de la partie
-		 * @return le GameControler
-		 */
-		public GameController getGc() {
-			return gc;
-		}
-
-		/**Getter pour le nombre minimal de joueur
-		 * @return le nombre minimal des joueurs
-		 */
-		public static int getNombreMinimalJoueur() {
-			return NOMBRE_MINIMAL_JOUEUR;
-		}
-		/**Getter pour le nombre maximal des joueurs
-		 * @return le nombre maximal des joueurs
-		 */
-		public static int getNombreMaximalJoueur() {
-			return NOMBRE_MAXIMAL_JOUEUR;
-		}
-		/**Getter pour l'age des joueurs
-		 * @return l'age du joueur
-		 */
-		public static int getAgeJoueur() {
-			return AGE_JOUEUR;
-		}
-
-		public int getNombreBot(){
-			return nombreBot;
-		}
-
-		public void setNombreBot(int nb){
-			nombreBot = nb;
-		}
-		
-		public String getStrategie(){
-			return strategie;
-		}
-
-		public void setStrategie(String strat) {
-			strategie = strat;
-		}
-
-		public int getNombreHumain() {
-			return nombreHumain;
-		}
-
-		public void setNombreHumain(int nb){
-			nombreHumain = nb;
-		}
-
-		public LinkedList<String> getListeNomHumain() {
-			return listeNomHumain;
-		}
-		
-		public void setListeNomHumain(LinkedList<String> liste) {
-			listeNomHumain = liste;
-		}
-
-		public LinkedList<Integer> getListeAgeHumain(){
-			return listeAgeHumain;
-		}
-		
-		public void setListeAgeHumain(LinkedList<Integer> liste) {
-			listeAgeHumain = liste;
-		}
-
-		public int getIndexHumain() {
-			return indexHumain;
-		}
-
-		public void setIndexHumain(int index) {
-			indexHumain = index;
-		}
-		
-		public PanelMenuPrincipale getPanelMenuPrincipale() {
-			return panelMenuPrincipale;
-		}
-
-		public void setPanelMenuPrincipale(PanelMenuPrincipale panelMenuPrincipale) {
-			this.panelMenuPrincipale = panelMenuPrincipale;
-		}
-
-		public PanelAjoutBot getPanelAjoutBot() {
-			return panelAjoutBot;
-		}
-
-		public void setPanelAjoutBot(PanelAjoutBot panelAjoutBot) {
-			this.panelAjoutBot = panelAjoutBot;
-		}
-
-		public PanelAjoutDifficulte getPanelAjoutDifficulte() {
-			return panelAjoutDifficulte;
-		}
-
-		public void setPanelAjoutDifficulte(PanelAjoutDifficulte panelAjoutDifficulte) {
-			this.panelAjoutDifficulte = panelAjoutDifficulte;
-		}
-
-		public PanelAjoutHumain getPanelAjoutHumain() {
-			return panelAjoutHumain;
-		}
-
-		public void setPanelAjoutHumain(PanelAjoutHumain panelAjoutHumain) {
-			this.panelAjoutHumain = panelAjoutHumain;
-		}
-
-		public PanelAjoutInfoHumain getPanelAjoutInfoHumain() {
-			return panelAjoutInfoHumain;
-		}
-
-		public void setPanelAjoutInfoHumain(PanelAjoutInfoHumain panelAjoutInfoHumain) {
-			this.panelAjoutInfoHumain = panelAjoutInfoHumain;
-		}
-
-		public PanelConfirmation getPanelConfirmation() {
-			return panelConfirmation;
-		}
-
-		public void setPanelConfirmation(PanelConfirmation panelConfirmation) {
-			this.panelConfirmation = panelConfirmation;
-		}
-
-		public LinkedList<String> getListeNomBot() {
-			return listeNomBot;
-		}
-
-		public void setListeNomBot(LinkedList<String> listeNomBots) {
-			this.listeNomBot = listeNomBots;
+	/**Methode pour revenir au panel MenuPrincipale*/
+	public void retourMenuPrincipale() {
+		int option = JOptionPane.showConfirmDialog(null, "Voulez vous revenir au menu principal ?", "Revenir au Menu Principal", 
+				JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, logo);
+		if(option == JOptionPane.OK_OPTION){
+			frame.setTitle("Client Pandocreon Divinae");
+			frame.setContentPane(panelMenuPrincipale);
+			frame.repaint();
+			frame.validate();
+			resetDonnees();
 		}
 	}
+	
+	/**Methode qui permet de reinitialiser les données*/
+	public void resetDonnees(){
+		setNombreHumain(0);
+		setNombreBot(0);
+		setStrategie("");
+		setListeNomBot(new LinkedList<String>());
+		setListeNomHumain(new LinkedList<String>());
+		setListeAgeHumain(new LinkedList<Integer>());
+		setIndexHumain(1);
+	}
+
+	/**Getter Jframe
+	 * @return la fenetre graphique
+	 */
+	public JFrame getFenetre(){
+		return frame;
+	}
+
+	/**Getter nombre de joueur
+	 * @return le nombre de joueur choisi pour demarrer la partie
+	 */
+	public int getNombreJoueurs(){
+		return this.getNombreBot()+this.getIndexHumain();
+	}
+
+	/**Getter pour le controller de la partie
+	 * @return le GameControler
+	 */
+	public GameController getGc() {
+		return gc;
+	}
+
+	/**Getter pour le nombre minimal de joueur
+	 * @return le nombre minimal des joueurs
+	 */
+	public static int getNombreMinimalJoueur() {
+		return NOMBRE_MINIMAL_JOUEUR;
+	}
+	/**Getter pour le nombre maximal des joueurs
+	 * @return le nombre maximal des joueurs
+	 */
+	public static int getNombreMaximalJoueur() {
+		return NOMBRE_MAXIMAL_JOUEUR;
+	}
+	/**Getter pour l'age des joueurs
+	 * @return l'age du joueur
+	 */
+	public static int getAgeJoueur() {
+		return AGE_JOUEUR;
+	}
+
+	public int getNombreBot(){
+		return nombreBot;
+	}
+
+	public void setNombreBot(int nb){
+		nombreBot = nb;
+	}
+
+	public String getStrategie(){
+		return strategie;
+	}
+
+	public void setStrategie(String strat) {
+		strategie = strat;
+	}
+
+	public int getNombreHumain() {
+		return nombreHumain;
+	}
+
+	public void setNombreHumain(int nb){
+		nombreHumain = nb;
+	}
+
+	public LinkedList<String> getListeNomHumain() {
+		return listeNomHumain;
+	}
+
+	public void setListeNomHumain(LinkedList<String> liste) {
+		listeNomHumain = liste;
+	}
+
+	public LinkedList<Integer> getListeAgeHumain(){
+		return listeAgeHumain;
+	}
+
+	public void setListeAgeHumain(LinkedList<Integer> liste) {
+		listeAgeHumain = liste;
+	}
+
+	public int getIndexHumain() {
+		return indexHumain;
+	}
+
+	public void setIndexHumain(int index) {
+		indexHumain = index;
+	}
+
+	public PanelMenuPrincipale getPanelMenuPrincipale() {
+		return panelMenuPrincipale;
+	}
+
+	public void setPanelMenuPrincipale(PanelMenuPrincipale panelMenuPrincipale) {
+		this.panelMenuPrincipale = panelMenuPrincipale;
+	}
+
+	public PanelAjoutBot getPanelAjoutBot() {
+		return panelAjoutBot;
+	}
+
+	public void setPanelAjoutBot(PanelAjoutBot panelAjoutBot) {
+		this.panelAjoutBot = panelAjoutBot;
+	}
+
+	public PanelAjoutDifficulte getPanelAjoutDifficulte() {
+		return panelAjoutDifficulte;
+	}
+
+	public void setPanelAjoutDifficulte(PanelAjoutDifficulte panelAjoutDifficulte) {
+		this.panelAjoutDifficulte = panelAjoutDifficulte;
+	}
+
+	public PanelAjoutHumain getPanelAjoutHumain() {
+		return panelAjoutHumain;
+	}
+
+	public void setPanelAjoutHumain(PanelAjoutHumain panelAjoutHumain) {
+		this.panelAjoutHumain = panelAjoutHumain;
+	}
+
+	public PanelAjoutInfoHumain getPanelAjoutInfoHumain() {
+		return panelAjoutInfoHumain;
+	}
+
+	public void setPanelAjoutInfoHumain(PanelAjoutInfoHumain panelAjoutInfoHumain) {
+		this.panelAjoutInfoHumain = panelAjoutInfoHumain;
+	}
+
+	public PanelConfirmation getPanelConfirmation() {
+		return panelConfirmation;
+	}
+
+	public void setPanelConfirmation(PanelConfirmation panelConfirmation) {
+		this.panelConfirmation = panelConfirmation;
+	}
+
+	public LinkedList<String> getListeNomBot() {
+		return listeNomBot;
+	}
+
+	public void setListeNomBot(LinkedList<String> listeNomBots) {
+		this.listeNomBot = listeNomBots;
+	}
+}
