273c00ab9cf6ffa8604890f5b0c285d433c5b4a2
Noga Lucas <luc4snoga@gmail.com>
2017-01-18 23:38:27 +0100

    Ajout des commentaires

--- Code/src/view/ihm/client/Client.java
+++ Code/src/view/ihm/client/Client.java
@@ -11,7 +11,13 @@ import controller.GameController;
 public class Client{
 
 	/**Attribut represennat les composants de l'interface graphique*/
-	private JFrame frame = new JFrame();
+	private JFrame frame = new JFrame();	
+	private PanelMenuPrincipale panelMenuPrincipale;
+	private PanelAjoutBot panelAjoutBot;
+	private PanelAjoutDifficulte panelAjoutDifficulte;
+	private PanelAjoutHumain panelAjoutHumain;
+	private PanelAjoutInfoHumain panelAjoutInfoHumain;
+	private PanelConfirmation panelConfirmation;
 
 	/**logo du jeu*/
 	private ImageIcon logo = new ImageIcon("images/logo.png");
@@ -42,26 +48,11 @@ public class Client{
 	/*Attribut representant les ages de humain*/
 	private LinkedList<Integer> listeAgeHumain = new LinkedList<Integer>();
 
-	/**attribut representant le numero du joueur*/
+	/**Attribut representant le numero du joueur*/
 	private int indexHumain = 1;
-
-	private PanelMenuPrincipale panelMenuPrincipale;
-
-	private PanelAjoutBot panelAjoutBot;
-
-	private PanelAjoutDifficulte panelAjoutDifficulte;
-
-	private PanelAjoutHumain panelAjoutHumain;
-
-	private PanelAjoutInfoHumain panelAjoutInfoHumain;
-
-	private PanelConfirmation panelConfirmation;
 	
+	/**Attribut permettant de démarrer la partie*/
 	private boolean initialize = false;
-
-	public boolean getInitialize(){
-		return initialize;
-	}
 	
 	/**Constructeur du client*/
 	public Client(){
@@ -74,13 +65,13 @@ public class Client{
 		frame.setVisible(true);	
 	}
 
-	//methode pour initialiser les composants de la fenetre
+	//methode permettant d'initialiser les composants de la fenetre
 	public void initialize(){
 		setPanelMenuPrincipale(new PanelMenuPrincipale(this));
 		//panelAjoutInfoHumain = new PanelAjoutInfoHumain(this);	
 	}
 
-	//methode qui permet d'ajouter les bots
+	//methode permettant d'ajouter les bots
 	public void ajouterBots() {
 		setPanelAjoutBot(new PanelAjoutBot(this));
 	}
@@ -90,29 +81,30 @@ public class Client{
 		setPanelAjoutDifficulte(new PanelAjoutDifficulte(this));
 	}
 
-	/**Methode qui permet d'ajouter les joueurs humains en fonction du nombre de joueur saisi*/
+	/**Methode permettant d'ajouter les joueurs humains en fonction du nombre de joueur saisi*/
 	public void ajouterJoueurHumain() {
 		setPanelAjoutHumain(new PanelAjoutHumain(this));
 	}
 
-	/**Methode Permettant d'ajouter le nom et l'age a un humain*/
+	/**Methode permettant d'ajouter le nom et l'age a un humain*/
 	public void ajouterInfoJoueurHumain() {
 		setPanelAjoutInfoHumain(new PanelAjoutInfoHumain(this));		
 	}
 
+	/**Methode permettant de verifier les donnees saisies et de les confirmer*/
 	public void confirmerPartie() {
 		setPanelConfirmation(new PanelConfirmation(this));
 	}
 
-	/**Methode pour lancer la partie*/
+	/**Methode permettant de lancer la partie*/
 	public void lancerPartie(){
 		creationPartie();
 	}
 
-	/**Methode pour instancier les joueurs et les ajouters dans le gameController*/
+	/**Methode permettant d'instancier les joueurs et les ajouter dans le gameController*/
 	public void creationPartie(){
 		initialize = true;
-		frame.setVisible(false);
+		frame.dispose();//permet de fermer la fenetre
 	}
 
 	/**Methode pour revenir au panel MenuPrincipale*/
@@ -128,7 +120,7 @@ public class Client{
 		}
 	}
 	
-	/**Methode qui permet de reinitialiser les données*/
+	/**Methode permettant de reinitialiser les données*/
 	public void resetDonnees(){
 		setNombreHumain(0);
 		setNombreBot(0);
@@ -178,55 +170,97 @@ public class Client{
 	public static int getAgeJoueur() {
 		return AGE_JOUEUR;
 	}
-
+	/**Getter pour le nombre de bot
+	 * @return le nombre de bot
+	 */
 	public int getNombreBot(){
 		return nombreBot;
 	}
-
+	/**Setter pour le nombre de bot
+	 * @param le nombre de bot
+	 */
 	public void setNombreBot(int nb){
 		nombreBot = nb;
 	}
-
+	/**Getter pour la strategie des bots
+	 * @return la strategie des bots
+	 */
 	public String getStrategie(){
 		return strategie;
 	}
-
+	/**Setter pour la strategie des bots
+	 * @param la strategie des bots
+	 */
 	public void setStrategie(String strat) {
 		strategie = strat;
 	}
-
+	/**Getter pour le nombre d'humain
+	 * @return le nombre d'humain
+	 */
 	public int getNombreHumain() {
 		return nombreHumain;
 	}
-
+	/**Setter pour le nombre d'humain
+	 * @param le nombre d'humain
+	 */
 	public void setNombreHumain(int nb){
 		nombreHumain = nb;
 	}
-
+	/**Getter pour les noms des humains
+	 * @return les noms des humains
+	 */
 	public LinkedList<String> getListeNomHumain() {
 		return listeNomHumain;
 	}
-
+	/**Setter pour les noms des humains
+	 * @param liste, les noms des humains
+	 */
 	public void setListeNomHumain(LinkedList<String> liste) {
 		listeNomHumain = liste;
 	}
-
+	/**Getter pour les ages des humains
+	 * @return les ages des humains
+	 */
 	public LinkedList<Integer> getListeAgeHumain(){
 		return listeAgeHumain;
 	}
-
+	/**Setter pour les ages des humains
+	 * @param liste les ages des humains
+	 */
 	public void setListeAgeHumain(LinkedList<Integer> liste) {
 		listeAgeHumain = liste;
 	}
-
+	/**Getter pour la position de l'humain
+	 * @return la position de l'humain
+	 */
 	public int getIndexHumain() {
 		return indexHumain;
 	}
-
+	/**Setter pour la position de l'humain
+	 * @param index la position de l'humain
+	 */
 	public void setIndexHumain(int index) {
 		indexHumain = index;
 	}
-
+	/**Getter pour les noms des bots
+	 * @return les noms des bots
+	 */
+	public LinkedList<String> getListeNomBot() {
+		return listeNomBot;
+	}
+	/**Setter pour les noms des bots
+	 * @param liste les noms des bots
+	 */
+	public void setListeNomBot(LinkedList<String> liste) {
+		this.listeNomBot = liste;
+	}
+	/**Setter pour la confirmation de la partie
+	 * @return la confirmation de la partie
+	 */
+	public boolean getInitialize(){
+		return initialize;
+	}
+	
 	public PanelMenuPrincipale getPanelMenuPrincipale() {
 		return panelMenuPrincipale;
 	}
@@ -273,13 +307,5 @@ public class Client{
 
 	public void setPanelConfirmation(PanelConfirmation panelConfirmation) {
 		this.panelConfirmation = panelConfirmation;
-	}
-
-	public LinkedList<String> getListeNomBot() {
-		return listeNomBot;
-	}
-
-	public void setListeNomBot(LinkedList<String> listeNomBots) {
-		this.listeNomBot = listeNomBots;
 	}
 }
